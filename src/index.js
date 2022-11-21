4f9c6a459a0ee92ec95a98ce5662ddbabe2bdef6
Vitor Torii <vitortorii@gmail.com>
2022-11-21 15:39:49 -0300

    refactor: port

--- src/index.js
+++ src/index.js
@@ -13,4 +13,5 @@ dotenv.config();
 app.use(accountRoutes);
 app.use(usersRoutes);
 
-app.listen(5000, () => console.log("Server running in port: 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
